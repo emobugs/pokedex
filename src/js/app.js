d5715091afbdb868e58a5acb09102ff06bd44e40
Emo <emotupev11@gmail.com>
2021-09-17 22:11:31 +0300

    feat: fetch data

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,24 @@
 import "../scss/app.scss";
 
 window.addEventListener("DOMContentLoaded", () => {
-  // This block will be executed once the page is loaded and ready
+	// This block will be executed once the page is loaded and ready
 
-  const ul = document.querySelector("ul");
+	const ul = document.querySelector("ul");
+
+	async function showPokemons() {
+		const fetchData = await fetch("https://pokeapi.co/api/v2/pokemon?limit=10");
+		const data = await fetchData.json();
+		const pokemons = data.results;
+
+		// console.log(pokemons);
+
+		pokemons.forEach((pokemon) => {
+			const li = document.createElement("li");
+			li.innerText = pokemon.name;
+
+			ul.appendChild(li);
+		});
+	}
+
+	showPokemons();
 });
